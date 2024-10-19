b0268ea6bb2467ef3aa4a00d555e853ec6178811
Fra <fra24eso@gmail.com>
2024-10-19 20:07:47 +0200

    Erntdankfest

--- src/components/thema/herbst/events.vue
+++ src/components/thema/herbst/events.vue
@@ -12,13 +12,31 @@ export default {
 <template>
   <div class="container">
     <Herbst></Herbst>
-    <h2>Veranstaltungen</h2>
-    <p>Entedankfest</p>
+    <div class="row">
+      <div class="col-12">
+        <h1>Event</h1>
+        <ul>
+          <li>
+            <h2>Bozen 19.10.2024 - Erntdankfest</h2>
+            <p>Dieses Wochenende <span class="nota">gab es</span> auf dem Waltherplatz das Erntedankfest. <br>
+              Es gab viele Stände <span class="nota">mit typischen</span> Südtiroler Speisen und Produkten.🍎🍏
+            
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+
   </div>
 </template>
 
 <style scoped>
-h2 {
-  margin-top: 20px;
+
+.nota{
+  color:brown;
+  font-weight: bold;
 }
+
+
 </style>
