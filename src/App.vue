cd2079d178aab08879572888e734b6067ee00a11
Fra <fra24eso@gmail.com>
2024-10-19 11:57:48 +0200

    setting project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import { RouterView, useRoute } from "vue-router";
+import FooterVue from './components/partials/FooterVue.vue';
+import NavbarVue from './components/partials/NavbarVue.vue';
+
+export default {
+  name: 'App',
+  components: {
+    FooterVue,
+    NavbarVue
+  },
+  setup() {
+    const route = useRoute();
+    return { route };
+  }
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <NavbarVue></NavbarVue>
+
+  <div class="content mt-5 pt-5">
+    <router-view />
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <FooterVue/>
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style></style>
---
+++ src/components/partials/NavbarVue.vue
@@ -0,0 +1,28 @@
+<script>
+import {RouterLink } from "vue-router";
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg fixed-top">
+    <div class="container-fluid">
+      <a class="navbar-brand px-5" href="/"><img src="./assets/img/logo/logo_completo.png" alt=""></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto gap-3">
+          <li class="nav-item">
+            <RouterLink exact-active-class="active" class="navlink" to="/1">1</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink exact-active-class="active" class="navlink" to="/2">2</RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+</template>
+
+<style></style>
