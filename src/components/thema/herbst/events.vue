6fa1c84d7efd0b611fcd84b566003842fda9a6a9
Fra <fra24eso@gmail.com>
2024-10-20 13:27:46 +0200

    gibt und sind

--- src/components/thema/herbst/events.vue
+++ src/components/thema/herbst/events.vue
@@ -1,10 +1,38 @@
 <script>
 import Herbst from './Herbst.vue';
+import Modal from './modals/ModHerbst.vue';
 
 export default {
   name: "events",
   components: {
-    Herbst
+    Herbst,
+    Modal
+  },
+  data() {
+    return {
+      showModal: false,
+      showExplanation: false,  // Aggiungi lo stato per gestire la visualizzazione della spiegazione
+      currentDescription: "",
+      notaDescriptions: {
+        war: "Das Fest fand am Wochenende statt.",
+        gab: "Es gab viele Stände mit Törggelen-Speisen.",
+        Törggelen: "Törggelen ist eine südtiroler Tradition im Herbst."
+      },
+      currentNota: ""
+    };
+  },
+  methods: {
+    openModal(nota) {
+      this.currentNota = nota;
+      this.currentDescription = this.notaDescriptions[nota] || "";
+      this.showModal = true;
+    },
+    saveDescription(newDescription) {
+      this.$set(this.notaDescriptions, this.currentNota, newDescription);
+    },
+    toggleExplanation() {
+      this.showExplanation = !this.showExplanation;  // Alterna la visualizzazione della spiegazione
+    }
   }
 };
 </script>
@@ -12,31 +40,90 @@ export default {
 <template>
   <div class="container">
     <Herbst></Herbst>
-    <div class="row">
-      <div class="col-12">
-        <h1>Event</h1>
-        <ul>
-          <li>
-            <h2>Bozen 19.10.2024 - Erntdankfest</h2>
-            <p>Dieses Wochenende <span class="nota">gab es</span> auf dem Waltherplatz das Erntedankfest. <br>
-              Es gab viele Stände <span class="nota">mit typischen</span> Südtiroler Speisen und Produkten.🍎🍏
-            
-            </p>
-          </li>
-        </ul>
+    <div class="row justify-content-center">
+      <h1>Event</h1>
+
+      <div class="col-12 col-lg-6">
+
+        <h2>Bozen 19.10.2024 - Erntdankfest</h2>
+        <p>
+          Dieses Wochenende <span class="nota" @click="openModal('war')">war</span> das Erntedankfest auf dem
+          Walterplatz in Bozen. <br>
+          Es <span class="nota" @click="openModal('gab')">gab</span> viele Stände mit typischen <span class="nota"
+            @click="openModal('Törggelen')">Törggelen</span>-Speisen und Produkten.🍎🍏<br>
+          Ich habe Kastanien🌰 und Glühwein🍷 gekauft und einen kleinen Snack gemacht.<br>
+          Es gab auch Bauernhoftiere: Schafe🐑 und Ziegen🐐.<br>
+          Es war ganz toll! Es gab auch eine Blaskapelle, die traditionelle Musik gespielt hat🎺🥁.
+          <br><br>
+          <button class="btn btn-info text-light fw-bold" @click="toggleExplanation">
+            {{ showExplanation ? 'Schließen' : 'Mehr lesen' }}
+          </button>
+        </p>
+
+        <hr>
+
+        <!-- La sezione della spiegazione è visibile solo se showExplanation è true -->
+        <div v-if="showExplanation">
+          <h3>gibt und sind</h3>
+          <a
+            href="https://www.deutschesinstitut.it/das-gibt-es-nicht/#:~:text=es%20gibt%20si%20riferisce%20all,non%20ci%20sta%20es%20gibt.">
+            Webressource</a>
+          <p>
+            <span class="dict">Es gibt</span> e <span class="dict">da ist/sind</span> in italiano si traducono entrambi
+            con “c’è/ci sono”. <br><br>
+
+            Generalmente in tedesco <span class="dict">es gibt</span> si riferisce all’esistenza di qualcosa.
+            <span class="dict">da ist/sind</span> alla presenza di qualcuno o qualcosa. <br><br>
+
+            <span class="dict">Es gibt</span> <br>
+            "Es gibt" significa che qualcosa esiste, che è disponibile o letteralmente che qualcosa “è dato”. Per questo
+            con una persona (che esiste) normalmente non ci sta es gibt.
+            <br>
+            Esempi:
+          <ol>
+            <li>Es gibt keine Gespenster. (I fantasmi non esistono.)</li>
+            <li> In der Welt gibt es viele Probleme. (Ci sono tanti problemi nel mondo.)</li>
+            <li>Hier gibt es viele gute Restaurants. (Qua ci sono tanti ristoranti buoni.)</li>
+            <li>In der Gartenstraße gibt es eine Bäckerei und einen Gemüsehändler. (Nella Gartenstraße ci sono (sono
+              disponibili) un forno e un ortolano.)</li>
+            <li>Heute gibt es Lasagne zum Mittagessen. (Oggi ci sono le lasagne a pranzo.)</li>
+          </ol>
+          <br>
+
+          <span class="dict">da ist/sind</span> <br>
+          "Da ist/da sind" significa che qualcosa o qualcuno è presente, a volte si riferisce anche alla posizione. <br>
+          Con le persone si usa quasi sempre "da ist/da sind". <br>
+          Esempi:
+          <br>
+          <ol>
+            <li>Ist Lisa da? (Lisa c’è?)</li>
+            <li>Da sind fünf Personen vor der Tür. (Ci sono cinque persone alla porta.)</li>
+            <li>Da hinten sind Freunde von mir! (Laggiù ci sono degli amici miei!)</li>
+          </ol>
+          A volte entrambe le forme sono possibili, a seconda di quello che si vuole sottinendere:
+          <ol>
+            <li>Gibt es hier eine Bank? – C’è una banca qua vicino? È disponibile?</li>
+            <li>Ist hier eine Bank? – C’è una banca qua vicino? Dove si trova?</li>
+          </ol>
+          </p>
+          <hr>
+        </div>
       </div>
     </div>
 
-
+    <Modal v-if="showModal" :isVisible="showModal" :description="currentDescription" @close="showModal = false"
+      @save="saveDescription" />
   </div>
 </template>
 
 <style scoped>
-
-.nota{
-  color:brown;
+.nota {
+  color: brown;
   font-weight: bold;
 }
 
-
+.dict {
+  color: rgb(255, 140, 0);
+  font-weight: bold;
+}
 </style>
---
+++ src/components/thema/herbst/modals/ModHerbst.vue
@@ -0,0 +1,54 @@
+<!-- Modal.vue -->
+
+<script>
+export default {
+    name: "Modal",
+    props: {
+        isVisible: Boolean,
+        description: String
+    },
+    methods: {
+        closeModal() {
+            this.$emit("close");
+        }
+    }
+};
+</script>
+
+<template>
+    <div v-if="isVisible" class="modal text-dark" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content text-center">
+                <div class="modal-body">
+                    <p>{{ description }}</p>
+                </div>
+                <div class="modal-footer justify-content-center">
+                    <button type="button" class="btn btn-primary" @click="closeModal">Schließen</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.modal {
+    display: block;
+    background-color: rgba(0, 0, 0, 0.5);
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.modal-dialog {
+    max-width: 400px;
+}
+
+.modal-content {
+    padding: 20px;
+}
+</style>
