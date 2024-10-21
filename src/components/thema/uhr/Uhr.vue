05c72e09102111c14be4fe650c44f6bd1c253688
Fra <fra24eso@gmail.com>
2024-10-21 15:53:19 +0200

    git add dist

--- src/components/thema/uhr/Uhr.vue
+++ src/components/thema/uhr/Uhr.vue
@@ -5,6 +5,9 @@
 <template>
     <div class="container">
         <h1>Uhr</h1>
+        <p>
+            test
+        </p>
         <img src="../../../assets/img/uhr/uhr.jpg" alt="">
         <img src="../../../assets/img/uhr/uhr_2.png" alt="">
     </div>
